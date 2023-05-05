faefa37219458093b73c35b14a2b2b154ea91a97
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2023-05-05 12:46:44 +1200

    Basic Data interface

--- index.py
+++ index.py
@@ -3,12 +3,29 @@ from tkinter import messagebox
 
 allData = [
     {
+      "Customer Name" : "Customer Name",
+      "Reciept Number" : "Reciept Number",
+      "Item Hired" : "Item Hired",
+      "Number Hired": "Number Hired"
+    },
+    {
       "Customer Name" : "Kim",
       "Reciept Number" : 12345678,
       "Item Hired" : "Spoon",
       "Number Hired": 12
     }
   ]
+
+def displayData():
+    dataWindow.deiconify()
+    row = 0
+    for obj in allData:
+        Label(dataWindow, text=obj.get("Customer Name")).grid(padx=10,column=0, row=row, sticky=W)
+        Label(dataWindow, text=obj.get("Reciept Number")).grid(padx=10,column=1, row=row, sticky=W)
+        Label(dataWindow, text=obj.get("Item Hired")).grid(padx=10,column=2, row=row, sticky=W)
+        Label(dataWindow, text=obj.get("Number Hired")).grid(padx=10,column=3, row=row, sticky=W)
+        Button(dataWindow, text="Delete").grid(padx=10,column=4, row=row, sticky=W)
+        row += 1
 
 main = Tk()
 dataWindow = Toplevel()
