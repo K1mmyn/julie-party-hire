105939fe4c6bc43bcc7d7ad64528912bd664ea5d
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2023-05-12 14:36:46 +1200

    Beginning of styling

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import random
 
 allData = []
 
 def submitData():
     
-    # Getting Data from Entry Boxes
+    # Getting Data from ttk.Entry Boxes
     
     # Data Validation
-    # TODO create a hide window button on data entry
+    # TODO create a hide window ttk.button on data ttk.entry
 
     while True:
 
@@ -47,7 +48,7 @@ def submitData():
             messagebox.showerror(title="Reciept Number Error", message="Item Hired must only include numbers")
             break
 
-    # * New Entry Creation
+    # * New ttk.Entry Creation
 
         userEntry = dict(
             CustomerFirstName = FirstName, 
@@ -61,10 +62,10 @@ def submitData():
         allData.append(userEntry)
         displayData()
 
-    # * Clearing Entry Boxes
+    # * Clearing ttk.Entry Boxes
 
         for widget in main.winfo_children():
-            if isinstance(widget, Entry):
+            if isinstance(widget, ttk.Entry):
                 widget.delete(0, END)
             itemHiredVar.set("...")
 
@@ -75,7 +76,7 @@ def deleteEntry(id):
     # * askyesno return either a truthy value or a falsey value so if askyesno returns a truthy value we run the 
     # * Delete function 
 
-    if messagebox.askyesno(message="Do you want to delete this Entry?", title="Delete Confirmation"):
+    if messagebox.askyesno(message="Do you want to delete this ttk.Entry?", title="Delete Confirmation"):
 
         global allData
         newArr = []
@@ -96,12 +97,12 @@ def displayData():
         widget.destroy()
 
     # Display Header for Data 
-    Label(dataWindow, text="First Name").grid(padx=10,column=0, row=0, sticky=W)
-    Label(dataWindow, text="Last Name").grid(padx=10,column=1, row=0, sticky=W)
-    Label(dataWindow, text="Reciept Number").grid(padx=10,column=2, row=0, sticky=W)
-    Label(dataWindow, text="Item Hired").grid(padx=10,column=3, row=0, sticky=W)
-    Label(dataWindow, text="Number Hired").grid(padx=10,column=4, row=0, sticky=W)
-    Button(dataWindow, text="Hide Window", command=lambda: hideWindow(dataWindow)).grid(padx=10,column=5, row=0, sticky=W)
+    ttk.Label(dataWindow, text="First Name").grid(padx=10,column=0, row=0, sticky=W)
+    ttk.Label(dataWindow, text="Last Name").grid(padx=10,column=1, row=0, sticky=W)
+    ttk.Label(dataWindow, text="Reciept Number").grid(padx=10,column=2, row=0, sticky=W)
+    ttk.Label(dataWindow, text="Item Hired").grid(padx=10,column=3, row=0, sticky=W)
+    ttk.Label(dataWindow, text="Number Hired").grid(padx=10,column=4, row=0, sticky=W)
+    ttk.Button(dataWindow, text="Hide Window", command=lambda: hideWindow(dataWindow)).grid(padx=10,column=5, row=0, sticky=W)
 
     # Revealing Data Window
     dataWindow.deiconify()
@@ -110,13 +111,13 @@ def displayData():
 
     # Printing Data for allData
     for obj in allData:
-        Label(dataWindow, text=obj.get("CustomerFirstName")).grid(padx=10,column=0, row=row, sticky=W)
-        Label(dataWindow, text=obj.get("CustomerLastName")).grid(padx=10,column=1, row=row, sticky=W)
-        Label(dataWindow, text=obj.get("RecieptNumber")).grid(padx=10,column=2, row=row, sticky=W)
-        Label(dataWindow, text=obj.get("ItemHired")).grid(padx=10,column=3, row=row, sticky=W)
-        Label(dataWindow, text=obj.get("NumberHired")).grid(padx=10,column=4, row=row, sticky=W)
+        ttk.Label(dataWindow, text=obj.get("CustomerFirstName")).grid(padx=10,column=0, row=row, sticky=W)
+        ttk.Label(dataWindow, text=obj.get("CustomerLastName")).grid(padx=10,column=1, row=row, sticky=W)
+        ttk.Label(dataWindow, text=obj.get("RecieptNumber")).grid(padx=10,column=2, row=row, sticky=W)
+        ttk.Label(dataWindow, text=obj.get("ItemHired")).grid(padx=10,column=3, row=row, sticky=W)
+        ttk.Label(dataWindow, text=obj.get("NumberHired")).grid(padx=10,column=4, row=row, sticky=W)
 
-        Button(dataWindow, text="Delete", command=lambda d=obj.get('id'): deleteEntry(d)).grid(padx=10,column=5, row=row, sticky=W)
+        ttk.Button(dataWindow, text="Delete", command=lambda d=obj.get('id'): deletettk.Entry(d)).grid(padx=10,column=5, row=row, sticky=W)
 
         row += 1
 
@@ -137,6 +138,9 @@ items = [
 main = Tk()
 main.title("Julie Party Hire")
 
+style = ttk.Style(main)
+style.theme_use("aqua")
+
 # Data Window
 dataWindow = Toplevel()
 
@@ -147,17 +151,17 @@ hideWindow(dataWindow)
 main.geometry("550x450")
 dataWindow.geometry("650x450")
 
-Label(main, text="First Name").grid(column=0, row=0, sticky=W)
-firstName = Entry(main, width=17)
+ttk.Label(main, text="First Name").grid(column=0, row=0, sticky=W)
+firstName = ttk.Entry(main, width=17)
 firstName.grid(column=1, row=0)
 
-Label(main, text="Last Name").grid(column=0, row=1, sticky=W)
-lastName = Entry(main, width=17)
+ttk.Label(main, text="Last Name").grid(column=0, row=1, sticky=W)
+lastName = ttk.Entry(main, width=17)
 lastName.grid(column=1, row=1)
 
 # Recipt Number
-Label(main, text="Receipt Number").grid(column=0, row=2, sticky=W)
-recieptNumber = Entry(main, width=17)
+ttk.Label(main, text="Receipt Number").grid(column=0, row=2, sticky=W)
+recieptNumber = ttk.Entry(main, width=17)
 recieptNumber.grid(column=1, row=2)
 
 # Item Hired
@@ -166,17 +170,17 @@ itemHiredVar.set("...")
 
 itemHiredDropDown = OptionMenu(main, itemHiredVar, *items)
 itemHiredDropDown.configure(width=11)
-Label(main, text="Item Hired").grid(column=0, row=3, sticky=W)
+ttk.Label(main, text="Item Hired").grid(column=0, row=3, sticky=W)
 itemHiredDropDown.grid(column=1, row=3)
 
 # Number Hired
-Label(main, text="Number Hired").grid(column=0, row=4, sticky=W)
-numberHired = Entry(main, width=17)
+ttk.Label(main, text="Number Hired").grid(column=0, row=4, sticky=W)
+numberHired = ttk.Entry(main, width=17)
 numberHired.grid(column=1, row=4)
 
 # Show Data & Add Data
 
-Button(main, text="Show Entries", command=displayData).grid(column=0, row=5, sticky=W)
-Button(main, text="Submit Entry", command=submitData).grid(column=1, row=5, sticky=W)
+ttk.Button(main, text="Show Entries", command=displayData).grid(column=0, row=5, sticky=W)
+ttk.Button(main, text="Submit Entry", command=submitData).grid(column=1, row=5, sticky=W)
 
 main.mainloop()
